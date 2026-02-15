204ccce221cad964676e7af38605b5a653f47d4e
Altay BRUSAN <altay.brusan@oklidis.com.tr>
2026-02-15 17:36:08 +0300

    Update Docker config, add healthchecks, minor code cleanup
    - Refactored docker-compose.yml: added env vars, healthchecks for DB/Redis, switched to named volume, adjusted exposed ports.
    - Simplified docker-compose.override.yml to empty services.
    - Updated Dockerfile to copy additional project files for build.
    - Suppressed CA1873 warning in RequestLoggingPipelineBehavior.
    - Cleaned up IDateTimeProvider interface property declaration.

--- 03.8 - Health Checks/evently/src/Common/Evently.Common.Application/Behaviors/RequestLoggingPipelineBehavior.cs
+++ 03.8 - Health Checks/evently/src/Common/Evently.Common.Application/Behaviors/RequestLoggingPipelineBehavior.cs
@@ -5,6 +5,9 @@ using Serilog.Context;
 
 namespace Evently.Common.Application.Behaviors;
 
+#pragma warning disable CA1873
+
+
 internal sealed class RequestLoggingPipelineBehavior<TRequest, TResponse>(
     ILogger<RequestLoggingPipelineBehavior<TRequest, TResponse>> logger)
     : IPipelineBehavior<TRequest, TResponse>
